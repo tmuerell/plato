f3ee81437ed8115821c16e3f4b0442b1e3335a79
Thorsten Mürell <thorsten@muerell.de>
2023-07-21 19:21:39 +0900

    Dependencies

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Plato
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Berlin"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :de
   end
 end
