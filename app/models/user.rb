2ad32ac7f020ad9c71fec5fa6a8b1f99c0a69a38
Thorsten Mürell <thorsten@muerell.de>
2023-07-22 16:55:36 +0200

    Add firstname and lastname from SSo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,10 @@ class User < ApplicationRecord
     def self.from_omniauth(auth)
       find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
         user.email = auth.info.email
+        user.firstname = auth.info.first_name
+        user.lastname = auth.info.last_name
         user.password = Devise.friendly_token[0, 20]
-        #user.name = auth.info.name   # assuming the user model has a name
-        #user.image = auth.info.image # assuming the user model has an image
-        # If you are using confirmable and the provider(s) you use validate emails,
-        # uncomment the line below to skip the confirmation emails.
-        # user.skip_confirmation!
+        #Roles here: auth.extra.raw_info.resource_access.plato.roles
       end
     end
   end
