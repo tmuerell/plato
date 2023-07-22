56165d66848a076e85022c4143e1fad6f4b5ba57
Thorsten Mürell <thorsten@muerell.de>
2023-07-23 00:01:03 +0200

    Added versioning for tickets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,9 @@ module ApplicationHelper
       flash_type.to_s
     end
   end
+
+  def markdown(text)
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
+    Markdown.new(text, *options).to_html.html_safe
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_184956) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_195801) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "ticket_id", null: false
@@ -84,6 +84,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_184956) do
     t.index ["current_project_id"], name: "index_users_on_current_project_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object", limit: 1073741823
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "comments", "tickets"
