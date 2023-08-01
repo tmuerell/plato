2c650929201c77a8effc1e6fa834039338018345
Thorsten Mürell <thorsten@muerell.de>
2023-08-01 16:14:52 +0200

    Styling, pagination

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,6 +7,7 @@ class TicketsController < ApplicationController
     if current_project
       @tickets = @tickets.where(project: current_project)
     end
+    @tickets = @tickets.order(:sequential_id).page params[:page]
   end
 
   def inbox
@@ -106,13 +107,13 @@ class TicketsController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ticket
-      @ticket = Ticket.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def ticket_params
-      params.require(:ticket).permit(:sequential_id, :title, :content, :project_id, :status, :priority, :customer_project_id, :creator_id, :assignee_id, tag_ids: [], files: [])
-    end
+  # Only allow a list of trusted parameters through.
+  def ticket_params
+    params.require(:ticket).permit(:sequential_id, :title, :content, :project_id, :status, :priority, :customer_project_id, :creator_id, :assignee_id, tag_ids: [], files: [])
+  end
 end
