4e77d3cf397df3745871d65f6ad0542ee165b04c
Thorsten Mürell <thorsten@muerell.de>
2023-08-03 22:27:21 +0200

    Some improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,9 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
+import { HighlightJS } from "highlight.js"
+
+document.addEventListener('turbo:load', (event) => {
+    console.log("High")
+    HighlightJS.highlightAll()
+});
