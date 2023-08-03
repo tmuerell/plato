a07b3660c71e564c30876fc9bf230c7973a99526
Thorsten Mürell <thorsten@muerell.de>
2023-08-03 22:53:42 +0200

    Added choices for easier boxes

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.8.0/es/index.js"
+pin "choices.js", to: "https://ga.jspm.io/npm:choices.js@10.2.0/public/assets/scripts/choices.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
