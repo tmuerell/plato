0d6a8a3eb21dc9d942fea250d9ca2ebdaad93b1c
Thorsten Mürell <thorsten@muerell.de>
2023-08-04 08:47:56 +0200

    Some tests refactored

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_213821) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_30_212903) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "api_tokens", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "token"
@@ -63,7 +91,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_213821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_board", default: false, null: false
+    t.boolean "is_area"
     t.index ["project_id"], name: "index_tags_on_project_id"
+  end
+
+  create_table "ticket_user_relationships", force: :cascade do |t|
+    t.integer "ticket_id", null: false
+    t.integer "user_id", null: false
+    t.string "relationship"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ticket_id"], name: "index_ticket_user_relationships_on_ticket_id"
+    t.index ["user_id"], name: "index_ticket_user_relationships_on_user_id"
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -119,12 +158,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_213821) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "api_tokens", "users"
   add_foreign_key "comments", "tickets"
   add_foreign_key "comments", "users", column: "creator_id"
   add_foreign_key "taggings", "tags"
   add_foreign_key "taggings", "users"
   add_foreign_key "tags", "projects"
+  add_foreign_key "ticket_user_relationships", "tickets"
+  add_foreign_key "ticket_user_relationships", "users"
   add_foreign_key "tickets", "customer_projects"
   add_foreign_key "tickets", "projects"
   add_foreign_key "tickets", "users", column: "assignee_id"
