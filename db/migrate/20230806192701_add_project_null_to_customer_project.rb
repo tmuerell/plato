6f0a3fc78a620b20bdee43d848e8a95ea3e76003
Thorsten Mürell <thorsten@muerell.de>
2023-08-06 21:27:19 +0200

    Added project to customer project

---
+++ db/migrate/20230806192701_add_project_null_to_customer_project.rb
@@ -0,0 +1,14 @@
+class AddProjectNullToCustomerProject < ActiveRecord::Migration[7.0]
+  def change
+    reversible do |dir|
+      dir.up do
+        CustomerProject.all.each do |tr|
+          tr.project = Project.first
+          tr.save
+        end
+
+        change_column_null :customer_projects, :project_id, false
+      end
+    end
+  end
+end
