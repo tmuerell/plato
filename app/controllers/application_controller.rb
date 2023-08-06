82442bbf66adc7a58f5b9bf741b27556c5953c26
Thorsten Mürell <thorsten@muerell.de>
2023-08-06 12:08:09 +0200

    Login restored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :set_paper_trail_whodunnit
-  protect_from_forgery
+  protect_from_forgery prepend: true
   before_action :authenticate_user_from_token!
   before_action :authenticate_user!
   layout :layout_by_resource
   helper_method :current_project
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
