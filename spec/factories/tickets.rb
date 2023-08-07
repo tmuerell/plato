31147f11d9996751b9a3839cea4b9bbe92cb949b
Thorsten Mürell <thorsten@muerell.de>
2023-08-07 06:27:14 +0200

    Added some more tests

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -3,8 +3,10 @@ FactoryBot.define do
     title { "Ticket" }
     content { "" }
     status { :new }
+    priority { :normal }
     association :creator, factory: :user
     association :assignee, factory: :user
     association :project
+    association :customer_project
   end
 end
---
+++ spec/features/user_roles_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "User roles", type: :feature do
+  before :each do
+    @project1 = create(:project, name: "Stinknormal")
+    @project2 = create(:project, name: "Area 51")
+    @user1 = create(:user, roles: [ "User" ], current_project: @project1)
+    @user2 = create(:user)
+    @customer_project = create(:customer_project, project: @project1)
+    @ticket1 = create(:ticket, project: @project1, title: 'Ticket stinknormal', customer_project: @customer_project)
+    @ticket2 = create(:ticket, project: @project2, title: 'Ticket Area 51', creator: @user2)
+    create(:user_project_role, user: @user1, project: @project1, role: :user)
+  end
+
+  it "allows me to see the projects I should see" do
+    sign_in_as(@user1)
+
+    visit '/projects'
+
+    expect(page).to have_content 'Stinknormal'
+    expect(page).to have_no_content 'Area 51'
+
+    visit '/tickets'
+
+    expect(page).to have_content 'Ticket stinknormal'
+    expect(page).to have_no_content 'Ticket Area 51'
+  end
+end
