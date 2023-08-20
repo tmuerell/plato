4c6112a9de20d3c051d04ce84842f304c7849d43
Zoran Zaric <zz@zoranzaric.de>
2023-08-20 07:36:57 +0200

    Make tags archivable
    closes #15

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_210004) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_051712) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -113,6 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_210004) do
     t.datetime "updated_at", null: false
     t.boolean "is_board", default: false, null: false
     t.boolean "is_area"
+    t.datetime "archived_at"
     t.index ["project_id"], name: "index_tags_on_project_id"
   end
 
