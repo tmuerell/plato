3342dd4608810e40bdf1f30ec3ff1fc496d8dea7
Zoran Zaric <zz@zoranzaric.de>
2023-08-22 16:36:21 +0200

    Implement copying ticket identifiers to clipboard

--- app/javascript/controllers/tickets_controller.js
+++ app/javascript/controllers/tickets_controller.js
@@ -13,4 +13,21 @@ export default class extends Controller {
   toggleLinkedTickets() {
     this.linkedTicketsTarget.classList.toggle("folded");
   }
+
+  copyToClipboard(e) {
+    e.preventDefault();
+    let element = e.target.closest("[data-ticket-identifier]");
+    if (element === undefined) {
+      console.error("ticket identifier not found");
+      return
+    }
+
+    let ticketIdentifier = element.dataset['ticketIdentifier'];
+    if (ticketIdentifier === undefined) {
+      console.error("ticket identifier not found");
+      return
+    }
+
+    navigator.clipboard.writeText(ticketIdentifier);
+  }
 }
