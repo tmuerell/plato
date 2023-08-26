62e7b4031caf0882147329546ea3005902f38734
Zoran Zaric <zz@zoranzaric.de>
2023-08-26 09:45:59 +0200

    Add data types for tag values

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,5 +1,9 @@
 class Tagging < ApplicationRecord
+  extend Enumerize
+
   belongs_to :user, optional: true
   belongs_to :tag
   belongs_to :taggable, polymorphic: true
+
+  enumerize :value_type, in: %i[string date]
 end
