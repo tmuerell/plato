9c6218b4e934ac71efab6c1af5d9714a33c569fc
Thorsten Mürell <thorsten@muerell.de>
2023-09-10 09:10:48 +0200

    Fixed wrong type name

--- db/migrate/20230828175351_add_specific_value_fields.rb
+++ db/migrate/20230828175351_add_specific_value_fields.rb
@@ -1,6 +1,6 @@
 class AddSpecificValueFields < ActiveRecord::Migration[7.0]
   def change
-    add_column :taggings, :number_value, :number
+    add_column :taggings, :number_value, :integer
     add_column :taggings, :date_value, :date
     add_reference :taggings, :user_value, null: true, foreign_key: { to_table: :users }
   end
