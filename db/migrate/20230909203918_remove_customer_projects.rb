50ce7add52c5186774dc46d5cb50f1c6dc54be03
Thorsten Mürell <thorsten@muerell.de>
2023-09-15 16:05:58 +0200

    Fixed type problem

--- db/migrate/20230909203918_remove_customer_projects.rb
+++ db/migrate/20230909203918_remove_customer_projects.rb
@@ -1,6 +1,6 @@
 class RemoveCustomerProjects < ActiveRecord::Migration[7.0]
   def change
-    remove_column :projects, :customer_project_id
+    #remove_column :projects, :customer_project_id
     remove_column :tickets, :customer_project_id
 
     drop_table :customer_projects
