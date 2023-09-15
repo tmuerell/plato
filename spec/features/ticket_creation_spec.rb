edb0b3464764128902df278678051caaf892e81b
Thorsten Mürell <thorsten@muerell.de>
2023-09-15 21:25:57 +0200

    Fixed test

--- spec/features/ticket_creation_spec.rb
+++ spec/features/ticket_creation_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.feature "TicketCreations", type: :feature do
   before :each do
     @project = create(:project)
-    @user1 = create(:user, roles: [ "User" ], current_project: @project)
+    @user1 = create(:user, roles: ["User"], current_project: @project)
     create(:user_project_role, user: @user1, project: @project, role: 'user')
   end
 
   it "allows me to create a ticket" do
     sign_in_as(@user1)
 
-    click_on "New ticket"
+    within find("nav") do
+      click_on "New ticket"
+    end
 
     fill_in 'Title', with: 'Das ist ein lustiges Ticket'
     click_on "Create Ticket"
