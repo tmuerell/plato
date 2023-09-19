f09aaf63e1b244dfc361d34788f63159f67123b3
Thorsten Mürell <thorsten@muerell.de>
2023-09-19 12:25:30 +0200

    Some more fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def gravatar_icon(user, additional_classes = [])
-    image_tag user.gravatar_url(secure: true, rating: 'R', size: 26, default: 'identicon'), class: ['clipped-circle'] + additional_classes, alt: user.name, title: user.name
+  def gravatar_icon(user, additional_classes = [], size = 26)
+    image_tag user.gravatar_url(secure: true, rating: 'R', size: size, default: 'identicon'), class: ['clipped-circle'] + additional_classes, alt: user.name, title: user.name
   end
 end
